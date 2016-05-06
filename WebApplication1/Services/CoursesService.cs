f02334936e248752abb7ee617214d84093c76b1d
jonsig <jonsig15@ru.is>
2016-05-06 14:33:02 +0000

    byrjun á CoursesServices

---
+++ WebApplication1/Services/CoursesService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using WebApplication1.Models.Entities
+
+namespace WebApplication1.Services
+{
+    public class CoursesService
+    {
+        private ApplicationDbContext _db;
+        public CoursesService()
+        {
+            _db = new ApplicationDbContext();
+        }
+        public void AddCourseToDB(Course newCourse)
+        {
+            _db.Courses.Add(newCourse);
+        }
+        public 
+    }
+}
