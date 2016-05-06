52cce473db2579474e232cceb831194d4224cff8
jonsig <jonsig15@ru.is>
2016-05-06 12:14:24 +0000

    Listum bætt við í Course.cs

--- WebApplication1/Models/Entities/Course.cs
+++ WebApplication1/Models/Entities/Course.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace WebApplication1.Models.Entities
 {
@@ -9,5 +10,18 @@ namespace WebApplication1.Models.Entities
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public List<ApplicationUser> Teachers { get; set; }
+        public List<ApplicationUser> Students { get; set; }
+
+        [Authorize("Admin")]
+        public void AddStudent(ApplicationUser student)
+        {
+            Students.Add(student);
+        }
+        [Authorize("Admin")]
+        public void AddTeachers(ApplicationUser teacher)
+        {
+            Teachers.Add(teacher);
+        }
     }
 }
